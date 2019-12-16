0a45b2eeb6fbaba4f8d28eea76bd8b9689587357
Mackenzie Smith <smack11231@gmail.com>
2019-12-15 21:30:50 -0700

    finalize style

--- finals.js
+++ finals.js
@@ -2,7 +2,7 @@ function sub() {
     var money = document.getElementById("money").value;
     var healthy = document.getElementById("healthy");
     var tasty = document.getElementById("tasty");
-    let elem = document.createElement('p');
+    let elem = document.createElement('h3');
     // let random = [taste1,taste2];
     let tasty2 = [" a burger for $2.50!"," a slice of pizza for $2.50!"," a tray of chicken nuggets for $2.50!"];
     var taste2 = tasty2[Math.floor(Math.random()*tasty2.length)];
@@ -14,17 +14,18 @@ function sub() {
     var side2 = sides2[Math.floor(Math.random()*sides2.length)];
     let choices = [taste2b +" and "+ side2, "a bag of bosco sticks for $2.75 " + side2, taste2b+" and "+side2, "a bag of breadsticks for $3.50 and a cookie for $0.75!", "a container of Mac & Cheese for $3.00!"]
     var choice = choices[Math.floor(Math.random()*choices.length)];
-    let choices2 = [taste2b +" and "+ side2, "a bag of bosco sticks for $2.75 " + side2, taste2b+" and "+side2, "a bag of breadsticks for $3.50 and a cookie for $0.75!", "a container of Mac & Cheese for $3.00"+side2, "a subway sandwich for $4.50!", taste2b+" and "+side2]
+    let choices2 = [taste2b +" and "+ side2, "a bag of bosco sticks for $2.75 and " + side2, taste2b+" and "+side2, "a bag of breadsticks for $3.50 and a cookie for $0.75!", "a container of Mac & Cheese for $3.00 "+side2, "a subway sandwich for $4.50!", taste2b+" and "+side2]
     var choice2 = choices2[Math.floor(Math.random()*choices2.length)];
     if(healthy.checked==true)
-        if (money>3.99)
+        if (money>3.99) {
             elem.innerHTML= "Try a small salad or fruit tray from the bar for $2.00, or a large one for $4.00!";
-        else if(money>=2 && money<4)
+        } else if(money>=2 && money<4) {
             elem.innerHTML= "Try a small salad or fruit from the bar for $2.00!";
-        else if(money<2 && money>0.01)
+        } else if(money<2 && money>0.01) {
             elem.innerHTML="Sorry you can't afford anything healthy. Try a bag of chips for $1.25 or a cookie for $0.75.";
-        else 
+        } else {
             alert("please enter a positive numerical value into the field.")
+        }
     else if(tasty.checked==true)
         if (money>=0.75 && money<1.25)
             elem.innerHTML= "Sorry, the only thing you can afford here is a $0.75 cookie.";
